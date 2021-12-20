79c71a9b28f962697e5cdf928cf850c4111c9daa
Krzysztof Strugiński <cagrin@gmail.com>
2021-12-21 00:09:51 +0100

    Add NavMenu test

---
+++ BlazorDemo.Tests/SharedObjects/NavMenuTests.cs
@@ -0,0 +1,28 @@
+namespace BlazorDemo.Tests.SharedObjects
+{
+    using BlazorDemo.Shared;
+    using Bunit;
+
+    [TestClass]
+    public class NavMenuTests
+    {
+        [TestMethod]
+        public void NavMenuShouldToggleWhenClicked()
+        {
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<NavMenu>();
+
+            var before = cut.FindAll("div")[1].HasAttribute("class");
+
+            cut.Find("button").Click();
+
+            var after = cut.FindAll("div")[1].HasAttribute("class");
+            var diffs = cut.GetChangesSinceFirstRender();
+            var diff = diffs.ShouldHaveSingleChange();
+
+            Assert.IsTrue(before);
+            Assert.IsFalse(after);
+            Assert.IsNotNull(diff);
+        }
+    }
+}
