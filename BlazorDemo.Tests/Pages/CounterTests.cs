e8a08eb08619df314bd9f30e30fcd94cac0528f1
Krzysztof Strugiński <cagrin@gmail.com>
2021-12-20 22:45:59 +0100

    Add first test in bUnit

---
+++ BlazorDemo.Tests/Pages/CounterTests.cs
@@ -0,0 +1,24 @@
+namespace BlazorDemo.Tests.Pages
+{
+    using BlazorDemo.Pages;
+    using Bunit;
+
+    [TestClass]
+    public class CounterTests
+    {
+        [TestMethod]
+        public void CounterShouldIncrementWhenClicked()
+        {
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
