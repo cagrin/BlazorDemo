4e0c06815a5bdd99724599b06edc4e56e166d7ed
Krzysztof Strugiński <cagrin@gmail.com>
2021-12-23 00:50:26 +0100

    Add WeatherForecastServiceShouldHasData test

--- BlazorDemo.Tests/Data/WeatherForecastServiceTests.cs
+++ BlazorDemo.Tests/Data/WeatherForecastServiceTests.cs
@@ -1,6 +1,9 @@
 namespace BlazorDemo.Tests.Data
 {
+    using System.Collections.Generic;
     using BlazorDemo.Data;
+    using Blazorise;
+    using Blazorise.DataGrid;
 
     [TestClass]
     public class WeatherForecastServiceTests
@@ -11,6 +14,24 @@ namespace BlazorDemo.Tests.Data
             var service = new WeatherForecastService();
             var isEmpty = service.Data == null;
             Assert.IsTrue(isEmpty);
+        }
+
+        [TestMethod]
+        public async Task WeatherForecastServiceShouldHasData()
+        {
+            DataGridReadDataMode readDataMode = DataGridReadDataMode.Paging;
+            IEnumerable<DataGridColumn<WeatherForecast>> columns = new List<DataGridColumn<WeatherForecast>>();
+            IList<DataGridColumn<WeatherForecast>> sortByColumns = new List<DataGridColumn<WeatherForecast>>();
+            int page = 1;
+            int pageSize = 5;
+            int virtualizeOffset = 0;
+            int virtualizeCount = 1;
+            var e = new DataGridReadDataEventArgs<WeatherForecast>(readDataMode, columns, sortByColumns, page, pageSize, virtualizeOffset, virtualizeCount);
+
+            var service = new WeatherForecastService();
+            await service.OnReadDataAsync(e);
+            var isEmpty = service.Data == null;
+            Assert.IsFalse(isEmpty);
         }
     }
 }
