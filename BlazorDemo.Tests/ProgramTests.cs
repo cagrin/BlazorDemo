151de853d00825b6f304c313353e34271fd919ad
Krzysztof Strugiński <cagrin@gmail.com>
2022-05-04 17:56:19 +0200

    Fix CancelAfter(2000) in test

--- BlazorDemo.Tests/ProgramTests.cs
+++ BlazorDemo.Tests/ProgramTests.cs
@@ -10,7 +10,7 @@ namespace BlazorDemo.Tests
         [TestMethod]
         public async Task ProgramMainShouldRunWithNullArgs()
         {
-            Program.CancellationTokenSource.CancelAfter(500);
+            Program.CancellationTokenSource.CancelAfter(2000);
 
             await Program.Main(args: null!);
         }
